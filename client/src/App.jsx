64d212d614ea9cad33c73ad6ab1e66220bd5cd68
Streafe <streafe@gmail.com>
2023-02-11 13:16:50 +0100

    Changed router to HashRouter to fix 404 on gh-pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  HashRouter,
+} from "react-router-dom";
 
 import { logo } from "./assets";
 import { Home, CreatePost } from "./pages";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <header
         className="w-full flex 
       justify-between items-center bg-white 
       sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]"
       >
-        <Link to="/dalle-clone/">
+        <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
         <Link
-          to="/dalle-clone/create-post"
+          to="/create-post"
           className="font-inter font-medium bg-[#6469ff] text-white px-4 py-4 rounded-md"
         >
           Create
@@ -24,11 +30,11 @@ function App() {
       </header>
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
-          <Route path="/dalle-clone/" element={<Home />} />
-          <Route path="/dalle-clone/create-post" element={<CreatePost />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
         </Routes>
       </main>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
