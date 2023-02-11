86977cf940e5934ef7ffa90aeb57c40787f833db
Streafe <streafe@gmail.com>
2023-02-11 03:50:05 +0100

    solution to github pages paths not correct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,11 @@ function App() {
       justify-between items-center bg-white 
       sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]"
       >
-        <Link to="/">
+        <Link to="/dalle-clone/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
         <Link
-          to="/create-post"
+          to="/dalle-clone/create-post"
           className="font-inter font-medium bg-[#6469ff] text-white px-4 py-4 rounded-md"
         >
           Create
@@ -24,8 +24,8 @@ function App() {
       </header>
       <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/dalle-clone/" element={<Home />} />
+          <Route path="/dalle-clone/create-post" element={<CreatePost />} />
         </Routes>
       </main>
     </BrowserRouter>
